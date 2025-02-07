6af65bba9d41d5b9442ce60c6c22e50ae9230960
José Páez <j_m_paez@hotmail.com>
2025-02-07 20:32:14 -0300

    Añadir solución y ejemplos de genéricos en .NET
    Se ha añadido un archivo de solución de Visual Studio (`entity_framework_y_linq.sln`) con la configuración necesaria para trabajar con Visual Studio 17.
    
    Se ha creado un archivo `1_ClasesYMetodosGenericos.cs` que contiene una clase `ClasesYMetodosGenericos` con métodos y clases que demuestran el uso de genéricos en .NET, incluyendo ejemplos y explicaciones detalladas.
    
    Se ha añadido un archivo de proyecto `m01.csproj` configurado para usar .NET 8.0.
    
    Se ha creado un archivo `Program.cs` que contiene la clase `Program` con el método `Main`, que ejecuta demostraciones de clases y métodos genéricos.
    
    Se ha añadido un archivo `MiClase.cs` que define una clase `MiClase` en el espacio de nombres `m01.clases`.

---
+++ m01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace m01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			// Clases y Metodos Genericos
+			ClasesYMetodosGenericos.Demos();
+
+			// Metodos de Extension
+
+			// Inferencia de Tipos
+
+			// Inicializadores de Objetos y Colecciones
+
+			// Tipos Anonimos
+
+			// Clases y Metodos Parciales
+
+		}
+	}
+}
