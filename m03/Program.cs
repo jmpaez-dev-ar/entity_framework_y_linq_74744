c8c78ad7d155e1d3e7625597447b6b633af67766
José Páez <j_m_paez@hotmail.com>
2025-03-19 05:15:15 -0300

    Actualiza llamadas a métodos de demostración de LINQ
    Se han eliminado comentarios de métodos de demostración de LINQ y se han activado las llamadas a estos métodos. Además, se han añadido nuevos métodos de demostración, ampliando así la funcionalidad del programa con más ejemplos de LINQ.

--- m03/Program.cs
+++ m03/Program.cs
@@ -4,15 +4,21 @@
 	{
 		static void Main(string[] args)
 		{
-			//_01_Linq_Basico.Demos();
-			//_02_Linq_Consultas.Demos();
-			//_03_Linq_Ejecucion_Diferida.Demos();
-			//_04_Linq_Ejecucion_Inmediata.Demos();
-			//_05_Linq_Subconsultas_Correlacionadas.Demos();
-			//_06_Linq_Subconsultas_No_Correlacionadas.Demos();
-			//_07_Linq_Operadores_Filtrado.Demos();
-			//_08_Linq_Operadores_Ordenamiento.Demos();
-			//_09_Linq_Operadores_Agrupacion.Demos();	
+			_01_Linq_Basico.Demos();
+			_02_Linq_Consultas.Demos();
+			_03_Linq_Ejecucion_Diferida.Demos();
+			_04_Linq_Ejecucion_Inmediata.Demos();
+			_05_Linq_Subconsultas_Correlacionadas.Demos();
+			_06_Linq_Subconsultas_No_Correlacionadas.Demos();
+			_07_Linq_Operadores_Filtrado.Demos();
+			_08_Linq_Operadores_Ordenamiento.Demos();
+			_09_Linq_Operadores_Agrupacion.Demos();
+			_10_Linq_Operadores_Cuantificacion.Demos();
+			_11_Linq_Operadores_Agregacion.Demos();
+			_12_Linq_Operadores_Conversion.Demos();
+			_13_Linq_Operadores_Particion.Demos();
+			_14_Linq_Operadores_Elemento.Demos();
+			_15_Linq_Operadores_Proyeccion.Demos();
 		}
 	}
 }
